f18be7268a4e9469ca988f51c9b3a0534689c082
Julian Ospina <julian.ospina83@gmail.com>
2023-10-09 16:32:42 -0500

    UseEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css'
 import { Test } from './components';
 
@@ -7,9 +7,26 @@ import { Test } from './components';
 function App() {
   // Ciclo de vida (Hooks) se crea -> se ejecuta -> se destruye
   const [a, setA] = useState(0);
+
+  const fetcher = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+    return res.json();
+  }
+
+
+  const fetchInformation = async () => {
+    const result = await fetcher();
+    setA(result);
+  }
+
+
+  useEffect(() => {
+    fetchInformation();
+  })
+
   return (
     <>
-    <button onClick={() => setA(a + 1)}> 
+    <button onClick={fetchInformation}> 
       Click me
     </button>
     <Test><>{a}</></Test> 
